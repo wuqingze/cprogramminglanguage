8368390a57cb530350788747a379741bb8a3d9a0
wuqingze <nongxiaolang@foxmail.com>
2020-08-01 09:41:37 +0800

    itoa recureve

--- chapter-4-functions-and-program-structure/9.itoa-recursive.c
+++ chapter-4-functions-and-program-structure/9.itoa-recursive.c
@@ -2,9 +2,17 @@
 #include <limits.h>
 
 void itoa(int n, char s[]);
+void _itoa(int n);
+void test01();
 
-int void main()
+int main()
 {
+    test01();
+    return 0;
+}
+
+void test00(){
+
 	char s[1024];
 	int n = INT_MAX;
 	itoa(n, s);
@@ -26,7 +34,44 @@ int void main()
 	itoa(n, s);
 	printf("s: %s, n: %d\n", s, n);
 
-	return 0;
+}
+
+void test01(){
+	int n = INT_MAX;
+	_itoa(n);
+	printf("\n");
+
+	n = -2147483647;
+	_itoa(n);
+	printf("\n");
+
+	n = -INT_MAX;
+	_itoa(n);
+	printf("\n");
+
+	n = -0;
+	_itoa(n);
+	printf("\n");
+
+	n = 0;
+	_itoa(n);
+	printf("\n");
+
+}
+
+void _itoa(int n){
+    int isMin = 0;
+    if(n<0){
+        printf("-");
+        if(n == -n){
+            n += 1;
+            isMin = 1;
+        }
+        n = -n;
+    }
+    if(n/10)
+        _itoa(n /10);
+    printf("%d", isMin?(n%10+1):(n%10));
 }
 
 void itoa(int n, char s[])

bf96b8510ded1d27e70237074a9c0938c46c1685
wuqingze <nongxiaolang@foxmail.com>
2020-08-20 06:23:52 +0800

    可变参数并实现_minprintf函数

--- chapter-7-input-and-output/minprintf.c
+++ chapter-7-input-and-output/minprintf.c
@@ -2,12 +2,49 @@
 #include <stdio.h>
 
 void minprintf(char *fmt, ...);
+void _minprintf(char *fmt, ...);
 
 int main()
 {
-	minprintf("int: %d, double: %f, string: %s\n", 1, 0.123, "Hello");
+	minprintf("int: %d, int: %d, double: %f, string: %s\n", 0, 1, 0.123, "Hello");
 
+	_minprintf("int: %d, int: %d, double: %f, string: %s\n", 0, 1, 0.123, "Hello");
 	return 0;
+}
+
+void _minprintf(char *fmt, ...){
+    char *c;
+    int intv;
+    double doublev;
+    char *stringv;
+
+    va_list ap;
+    va_start(ap, fmt);
+    for(c = fmt; *c; c++){
+        if(*c != '%'){
+            putchar(*c);
+            continue;
+        }
+
+        switch(*++c){
+            case 'd':
+                intv  = va_arg(ap, int);
+                printf("%d", intv);
+                break;
+            case 'f':
+                doublev = va_arg(ap, double);
+                printf("%f", doublev);
+                break;
+            case 's':
+                for(stringv = va_arg(ap, char*); *stringv; stringv++)
+                    putchar(*stringv);
+                break;
+            default:
+                putchar(*c);
+                break;
+        }
+    }
+    va_end(ap);
 }
 
 void minprintf(char *fmt, ...)

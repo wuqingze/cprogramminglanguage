4adeae7a643ceec4d205f37e68b6a407b538d11c
wuqingze <nongxiaolang@foxmail.com>
2020-08-23 20:34:54 +0800

    lseek file; append file by iseek lseek(fd, offset, pos) pos: 0->start; 2->end

--- chapter-8-the-unix-system-interface/appendfile.c
+++ chapter-8-the-unix-system-interface/appendfile.c
@@ -1,7 +1,18 @@
+#include <fcntl.h>
 #include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h> 
+#include <stdlib.h> 
 
 int main(int argc, char *argv[]){
+    void test00();
+    void test01();
+
+    test01();
+    return 0;
+}
+
+
+void test00(){
     FILE *f = fopen("t", "a");
     char *line = "hello";
     char *ptr;
@@ -13,5 +24,19 @@ int main(int argc, char *argv[]){
         }
         putc('\n', f);
     }
-    return 0;
+}
+
+void test01(){
+    int fp = open("t", O_WRONLY, 0);
+    lseek(fp, 0L, 2);
+    char s[] = "hello world";
+    write(fp, s, 11);
+}
+
+int get(int fd, long pos, char *buf, int n){
+    if(lseek(fd, pos, 0) >=0){
+        return read(fd, buf, n);
+    }else {
+        return -1;
+    }
 }
